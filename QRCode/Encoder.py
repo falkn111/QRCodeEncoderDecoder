b1ab4f88fad63194de8320b40cbccc6f21ce3015
Christopher Falkner <50120458+falkn111@users.noreply.github.com>
2022-04-03 15:31:56 -0400

    Delete QRCode directory

--- QRCode/Encoder.py
+++
@@ -1,7 +0,0 @@
-import qrcode
-
-dataName = input("Enter a link for your QR Code: ")
-image = qrcode.make(dataName) #QR Code stored in "image" variable with data inside brackets
-
-type(image)
-image.save("QRCode.png")
