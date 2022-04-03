15460ff191dd9f5dd1fe4c63c12ef662e0b3a86e
Christopher Falkner <50120458+falkn111@users.noreply.github.com>
2022-04-03 15:37:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template, url_for, redirect, request
 import os
 import cv2
+import qrcode
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
-    
-app.config["IMAGE_UPLOADS"] = "T:\VSCode Folder\FlaskQRCode"  #Need if we change directory. Change also in image.save and cv2.imread
+
+@app.route("/encoder")
+def encoder():
+    return render_template("encoder.html")
+
+@app.route('/encoded', methods=["GET", "POST"])
+def encoded():
+    output = request.form.to_dict()
+    name = output["name"]
+
+    image = qrcode.make(name) #QR Code stored in "image" variable with data inside brackets
+    type(image)
+    image.save("static/QRCode.jpg")
+    return render_template('encoded.html')
+
 
 @app.route("/decoder", methods=["GET", "POST"])
 def decoder():
@@ -19,11 +34,11 @@ def decoder():
         if request.files:           #More specifically, if we are trying to input files
 
             image = request.files["image"]
-            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename)) #Saves image in current directory
+            image.save('static/' +  image.filename) #Saves image in current directory
 
             print(image)    #Test purposes
 
-            image = cv2.imread(image.filename)      #Using the CV2 library, read image
+            image = cv2.imread('static/' + image.filename)      #Using the CV2 library, read image
             detector = cv2.QRCodeDetector()                      #Initalizing CV2 QRCode Detector to ensure image quality
             data, vertices_array, binary_qrcode = detector.detectAndDecode(image)   #Using the detector's Detect and Decode function
 
@@ -36,4 +51,4 @@ def decoder():
     return render_template("decoder.html", link=data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
